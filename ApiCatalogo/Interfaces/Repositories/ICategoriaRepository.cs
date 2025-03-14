fbb13d17a9d707088054495701804c55de86e0ce
Aislan Santos <aislan.santos@gmail.com>
2025-03-14 06:14:25 -0300

    feat - estrutura de Repositório generico

--- ApiCatalogo/Interfaces/Repositories/ICategoriaRepository.cs
+++ ApiCatalogo/Interfaces/Repositories/ICategoriaRepository.cs
@@ -1,17 +1,7 @@
 ﻿using ApiCatalogo.Models;
-using Microsoft.EntityFrameworkCore.Update.Internal;
 
 namespace ApiCatalogo.Interfaces.Repositories;
 
-public interface ICategoriaRepository
+public interface ICategoriaRepository : IRepository<Categoria>
 {
-    IEnumerable<Categoria> GetCategorias();
-
-    Categoria GetCategoria(int id);
-
-    Categoria Create(Categoria categoria);
-
-    Categoria Update(Categoria categoria);
-
-    Categoria Delete(int id);
 }
--- ApiCatalogo/Repositories/CategoriaRepository.cs
+++ ApiCatalogo/Repositories/CategoriaRepository.cs
@@ -1,58 +1,12 @@
 ﻿using ApiCatalogo.Interfaces.Repositories;
 using ApiCatalogo.Models;
 using APICatalogo.Context;
-using Microsoft.EntityFrameworkCore;
 
 namespace ApiCatalogo.Repositories;
 
-public class CategoriaRepository : ICategoriaRepository
+public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
 {
-    private readonly AppDbContext _context;
-
-    public CategoriaRepository(AppDbContext context)
+    public CategoriaRepository(AppDbContext context) : base(context)
     {
-        _context = context;
-    }
-
-    public Categoria Create(Categoria categoria)
-    {
-        ArgumentNullException.ThrowIfNull(categoria); // Forma mais moderna de fazer a validação de categoria is null, se for retorna uma exception.
-
-        _context.Categorias.Add(categoria);
-        _context.SaveChanges();
-
-        return categoria;
-    }
-
-    public Categoria Delete(int id)
-    {
-        var categoria = _context.Categorias.Find(id);
-
-        ArgumentNullException.ThrowIfNull(nameof(categoria));
-
-        _context.Categorias.Remove(categoria);
-        _context.SaveChanges();
-
-        return categoria;
-    }
-
-    public Categoria GetCategoria(int id)
-    {
-        return _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
-    }
-
-    public IEnumerable<Categoria> GetCategorias()
-    {
-        return _context.Categorias.ToList();
-    }
-
-    public Categoria Update(Categoria categoria)
-    {
-        ArgumentNullException.ThrowIfNull(nameof(categoria));
-
-        _context.Entry(categoria).State = EntityState.Modified;// o tratamento de update e o entry(reponsabilidade).state é identica no final das contas.
-        _context.SaveChanges();
-
-        return categoria;
     }
 }
